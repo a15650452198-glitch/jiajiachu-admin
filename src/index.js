44fc40857284b87b92527e65d4a38c4c0e87f572
王文超 <a15650452198@gmail.com>
2026-02-22 21:10:02 +0800

    build successful, deploy to vercel

--- src/index.js
+++ src/index.js
@@ -6,10 +6,11 @@ import reportWebVitals from './reportWebVitals';
 import cloudbase from '@cloudbase/js-sdk';
 
 const app = cloudbase.init({
-  env: 'cloud1-5g8unv4l1a3f77a1'   // 替换成你的云环境ID，例如 jiajiachu-prod-123abc
+  env: 'cloud1-5g8unv4l1a3f77a1',
 });
 
 export const db = app.database();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
@@ -17,7 +18,4 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
